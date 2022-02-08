18370064b49d0c4a1b194bed6f151f8d91720ff6
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-08 10:01:04 +0100

    Update 00.get_data.sh

--- 00.get_data.sh
+++ 00.get_data.sh
@@ -1,17 +1,18 @@
 # create folder structure and get PacBio HiFi data	
-mkdir -p Elephant_project/mEleMax1/genomic_data/pacbio
-cd Elephant_project/mEleMax1/genomic_data/pacbio
+mkdir -p ~/Elephant_project/mEleMax1/genomic_data/pacbio
+cd ~/Elephant_project/mEleMax1/genomic_data/pacbio
 aws s3 cp s3://genomeark/species/Elephas_maximus/mEleMax1/genomic_data/pacbio/ . --recursive --no-sign-request
-cd -
 
 # create folder structure and get Bionano data
-mkdir -p Elephant_project/mEleMax1/genomic_data/bionano
-cd Elephant_project/mEleMax1/genomic_data/bionano
+mkdir -p ~/Elephant_project/mEleMax1/genomic_data/bionano
+cd ~/Elephant_project/mEleMax1/genomic_data/bionano
 aws s3 cp s3://genomeark/species/Elephas_maximus/mEleMax1/genomic_data/bionano/ . --recursive --no-sign-request
-cd -
 
 # create folder structure and get HiC data
+mkdir -p ~/Elephant_project/mEleMax1/genomic_data/arima
+cd ~/Elephant_project/mEleMax1/genomic_data/arima
+aws s3 cp s3://genomeark/species/Elephas_maximus/mEleMax1/genomic_data/arima/ . --recursive --no-sign-request
 
 # create folder structure
-mkdir -p Elephant_project/mEleMax1/evaluation
-mkdir -p Elephant_project/mEleMax1/intermediates
+mkdir -p ~/Elephant_project/mEleMax1/evaluation
+mkdir -p ~/Elephant_project/mEleMax1/intermediates
