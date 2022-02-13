122e4a676704792de292b427de018c58c65b3223
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-13 19:12:09 +0100

    Update 3.Run_RepeatM.sh

--- extras/3.Run_RepeatM.sh
+++ extras/3.Run_RepeatM.sh
@@ -1,2 +1,2 @@
 source 2.repeat_variables.cnf
-sbatch --output=${OUT_DIR}/%x.%j.out --error=${OUT_DIR}/%x.%j.err slurm/RepeatM.job
+sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/%x.%j.out --error=${OUT_DIR}/%x.%j.err slurm/RepeatM.job
