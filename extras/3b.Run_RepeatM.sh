2ae645e4760fe99fa8d62f4e303958ff7f76dae7
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-17 18:23:54 +0100

    Update 3b.Run_RepeatM.sh

--- extras/3b.Run_RepeatM.sh
+++ extras/3b.Run_RepeatM.sh
@@ -11,13 +11,13 @@ mkdir -p "${OUT_DIR}/01_modeler"
 cd "${OUT_DIR}/01_modeler"
 ln -s $ASSEMBLY ${ASSEMBLY_NAME}.fa
 
-singularity exec --bind ${BIND_DIR}:${BIND_DIR} $TET_SIF \
+singularity exec --bind ${BIND_DIR}:${BIND_DIR} "${INSTALLATION_DIR}/tetools_latest.sif" \
 BuildDatabase -name ${ASSEMBLY_NAME} ${ASSEMBLY_NAME}.fa
 
 
 echo "=== 2/5. Starting RepeatModeler...  =================================================="
 
-singularity exec --bind ${BIND_DIR}:${BIND_DIR} $TET_SIF \
+singularity exec --bind ${BIND_DIR}:${BIND_DIR} "${INSTALLATION_DIR}/tetools_latest.sif" \
 RepeatModeler -database ${ASSEMBLY_NAME} -pa ${SLURM_CPUS_PER_TASK} -LTRStruct
 
 
@@ -27,7 +27,7 @@ mkdir -p "${OUT_DIR}/02_libraries"
 cd "${OUT_DIR}/02_libraries"
 
 #extract a species-specific FASTA library from the installed libraries
-singularity exec --bind ${BIND_DIR}:${BIND_DIR} --pwd /opt/RepeatMasker/Libraries/ $TET_SIF \
+singularity exec --bind ${BIND_DIR}:${BIND_DIR} --pwd /opt/RepeatMasker/Libraries/ "${INSTALLATION_DIR}/tetools_latest.sif" \
 famdb.py -i RepeatMaskerLib.h5 families --format fasta_name --include-class-in-name --ancestors --descendants "${SPECIES_NAME}" > ${ASSEMBLY_NAME}-rm.fa
 
 #combine libraries
@@ -40,17 +40,17 @@ mkdir -p "${OUT_DIR}/03_masker"
 cd "${OUT_DIR}/03_masker"
 ln -s $ASSEMBLY ${ASSEMBLY_NAME}.fa
 
-singularity exec --bind ${BIND_DIR}:${BIND_DIR} $TET_SIF \
+singularity exec --bind ${BIND_DIR}:${BIND_DIR} "${INSTALLATION_DIR}/tetools_latest.sif" \
 RepeatMasker -pa ${SLURM_CPUS_PER_TASK} -a -s -gccalc -xsmall -lib "${OUT_DIR}/02_libraries/${ASSEMBLY_NAME}_combined.fa" ${ASSEMBLY_NAME}.fa
 
 
 echo "=== 5/5. Plotting results...  ========================================================"
 
-singularity exec --bind ${BIND_DIR}:${BIND_DIR} $TET_SIF \
+singularity exec --bind ${BIND_DIR}:${BIND_DIR} "${INSTALLATION_DIR}/tetools_latest.sif" \
 calcDivergenceFromAlign.pl -s ${ASSEMBLY_NAME}.align.divsum -a ${ASSEMBLY_NAME}.align_with_div ${ASSEMBLY_NAME}.align
 
 VAR="$(grep -n total ${ASSEMBLY_NAME}.tbl| cut -f1 -d:)"
 SIZE="$(sed -n $VAR\p ${ASSEMBLY_NAME}.tbl | sed -e 's/ /\t/g' | cut -f3)"
 
-singularity exec --bind ${BIND_DIR}:${BIND_DIR} $TET_SIF \
+singularity exec --bind ${BIND_DIR}:${BIND_DIR} "${INSTALLATION_DIR}/tetools_latest.sif" \
 createRepeatLandscape.pl -div ${ASSEMBLY_NAME}.align.divsum -g ${SIZE} > ${ASSEMBLY_NAME}.align.divsum.html
