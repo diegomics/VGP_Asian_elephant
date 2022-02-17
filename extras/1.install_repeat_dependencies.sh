4a4affe7acbc6d63ebb9887fb357a6257c67e3a0
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-17 18:18:51 +0100

    Update 1.install_repeat_dependencies.sh

--- extras/1.install_repeat_dependencies.sh
+++ extras/1.install_repeat_dependencies.sh
@@ -1,5 +1,5 @@
 echo
-echo "* building singularity container in '/VGP_Asian_elephant/extras/'..."
+echo "* building singularity container in '../VGP_Asian_elephant/extras/'..."
 echo
 module load singularity
 singularity pull docker://dfam/tetools:latest
