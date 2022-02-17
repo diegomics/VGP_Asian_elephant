3fbfdc662d7f9af3c31c989ce12af467df7a10e5
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-17 21:40:56 +0100

    Update run_bionano.sh

--- run_bionano.sh
+++ run_bionano.sh
@@ -4,6 +4,6 @@ mkdir -p "${OUT_DIR}/${ASSEMBLY_NAME}"
 cd "${OUT_DIR}/${ASSEMBLY_NAME}"
 cp "${BIONANO_DIR}/*hybrid*" .
 ln -s $ASSEMBLY "${ASSEMBLY_NAME}.fasta"
-ln -s $CMAP "${CMAP_NAME}.cmap"
+ln -s $CMAP "DL1.cmap"
 
 bash submit_hybrid_scaffold_dle1_3.3.sh
