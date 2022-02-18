173aef48b28e3c891fcbc0b3aae060bf396b8e73
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-18 17:18:55 +0100

    Update run_bionano.sh

--- run_bionano.sh
+++ run_bionano.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 source bionano.cnf
 
 mkdir -p "${OUT_DIR}/${ASSEMBLY_NAME}/std_logs"
 cd "${OUT_DIR}/${ASSEMBLY_NAME}"
 cp "${BIONANO_DIR}/*hybrid*" .
 ln -s $ASSEMBLY "${ASSEMBLY_NAME}.fasta"
-ln -s $CMAP "DL1.cmap"
+ln -s $CMAP_FILE "DL1.cmap"
 
 bash submit_hybrid_scaffold_dle1_3.3.sh $NAME
 
@@ -16,21 +15,10 @@ bash submit_hybrid_scaffold_dle1_3.3.sh $NAME
 source ~/.bashrc
 conda activate bionano
 
-if [ -z $1 ]; then
-	echo "Usage: ./_submit_hybrid_scaffold_dle1.sh <name>"
-	echo -e "\t<name>: job name and output dir name"
-	echo -e "\tAssumes we have asm.fasta and DLE1.cmap in the same dir"
-	exit -1
-fi
-
-if [ -e $1 ]; then
-	echo "$1 already exists. Remove."
-	exit -1
-fi
 
 cpus=48
 mem=960g
-partition=be
+
 name=$1
 script="/scratch/ddepanis/Software/bionano/hybrid_scaffold_3.3.sh"
 #args="$1 DLE1 $VGP_PIPELINE/bionano/hybridScaffold_DLE1_HiC_config_3.3.xml"
@@ -41,6 +29,9 @@ mkdir -p logs
 log=$PWD/logs/$name.%A_%a.log
 
 
-sbatch  --cpus-per-task=$cpus --job-name=$name --mem=$mem --time=$walltime $script $args
+$script $args
 
-sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err slurm/arima.index1.job)
+sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --cpus-per-task=${CPUS} --mem=${MEM} --time=${MAX_TIME} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err
+
+
+slurm/arima.index1.job)
