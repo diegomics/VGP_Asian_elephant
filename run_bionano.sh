10ee0938fcc67798b2ce472bb9524a91330ed33d
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-18 17:53:43 +0100

    Update run_bionano.sh

--- run_bionano.sh
+++ run_bionano.sh
@@ -8,32 +8,9 @@ mkdir -p "${OUT_DIR}/${ASSEMBLY_NAME}/std_logs"
 cd "${OUT_DIR}/${ASSEMBLY_NAME}"
 cp "${BIONANO_DIR}/*hybrid*" .
 ln -s $ASSEMBLY "${ASSEMBLY_NAME}.fasta"
-ln -s $CMAP_FILE "DL1.cmap"
-
-bash submit_hybrid_scaffold_dle1_3.3.sh $NAME
-
-
+ln -s $CMAP_FILE DL1.cmap
 
 source ~/.bashrc
 conda activate bionano
 
-
-cpus=48
-mem=960g
-
-name=$1
-script="/scratch/ddepanis/Software/bionano/hybrid_scaffold_3.3.sh"
-#args="$1 DLE1 $VGP_PIPELINE/bionano/hybridScaffold_DLE1_HiC_config_3.3.xml"
-args="$1 DLE1 /scratch/ddepanis/Software/bionano/hybridScaffold_DLE1_config_3.3.xml"
-walltime=7-00:00:00
-
-mkdir -p logs
-log=$PWD/logs/$name.%A_%a.log
-
-
-$script $args
-
-sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --cpus-per-task=${CPUS} --mem=${MEM} --time=${MAX_TIME} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err
-
-
-slurm/arima.index1.job)
+sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --cpus-per-task=${CPUS} --mem=${MEM} --time=${MAX_TIME} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err bash "${BIONANO_DIR}/hybrid_scaffold_3.3.sh" ${ASSEMBLY_NAME} DLE1 "${BIONANO_DIR}/hybridScaffold_DLE1_config_3.3.xml"
