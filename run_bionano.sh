31bd647e4725920c8510152f359bb5581b7245aa
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-18 17:08:19 +0100

    Delete run_bionano.sh

--- run_bionano.sh
+++
@@ -1,9 +0,0 @@
-source bionano.cnf
-
-mkdir -p "${OUT_DIR}/${ASSEMBLY_NAME}/std_logs"
-cd "${OUT_DIR}/${ASSEMBLY_NAME}"
-cp "${BIONANO_DIR}/*hybrid*" .
-ln -s $ASSEMBLY "${ASSEMBLY_NAME}.fasta"
-ln -s $CMAP "DL1.cmap"
-
-bash submit_hybrid_scaffold_dle1_3.3.sh $NAME
