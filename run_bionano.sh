f44b87a7dfd270a2c20afe7edd9be7ac3216f767
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-21 21:20:14 +0100

    Update run_bionano.sh

--- run_bionano.sh
+++ run_bionano.sh
@@ -13,3 +13,6 @@ source ~/.bashrc
 conda activate bionano
 
 sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --cpus-per-task=${CPUS} --mem=${MEM} --time=${MAX_TIME} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err "${BIONANO_DIR}/hybrid_scaffold_3.3.sh" ${ASSEMBLY_NAME} DLE1 "${BIONANO_DIR}/hybridScaffold_DLE1_config_3.3.xml"
+
+sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err 
+
