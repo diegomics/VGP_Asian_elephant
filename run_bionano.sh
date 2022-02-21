63230bd46435b1cfc1d91fbe3aaa30c608accdc1
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-21 21:32:05 +0100

    Update run_bionano.sh

--- run_bionano.sh
+++ run_bionano.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# === STEP 0/: Get variables & create folders  ======================
 source bionano.cnf
 
 mkdir -p "${OUT_DIR}/${ASSEMBLY_NAME}/std_logs"
@@ -12,7 +13,11 @@ ln -s $CMAP_FILE DL1.cmap
 source ~/.bashrc
 conda activate bionano
 
+# === STEP 1/:  ======================
+BIONANO_JOB
 sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --cpus-per-task=${CPUS} --mem=${MEM} --time=${MAX_TIME} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err "${BIONANO_DIR}/hybrid_scaffold_3.3.sh" ${ASSEMBLY_NAME} DLE1 "${BIONANO_DIR}/hybridScaffold_DLE1_config_3.3.xml"
+BIONANO_JOB_ID=$(echo $BIONANO_JOB | cut -d ' ' -f4)
 
+# === STEP 2/:  ======================
 sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err trimNs.job
 
