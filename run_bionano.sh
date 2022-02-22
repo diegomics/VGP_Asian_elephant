c1a0cb0e41a06430c1cd94c3ce92f3da89906cf3
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-22 09:59:52 +0100

    Update run_bionano.sh

--- run_bionano.sh
+++ run_bionano.sh
@@ -5,7 +5,7 @@ source bionano.cnf
 
 mkdir -p "${OUT_DIR}/${ASSEMBLY_NAME}/std_logs"
 cd "${OUT_DIR}/${ASSEMBLY_NAME}"
-cp ${BIONANO_DIR}/hybrid* .
+cp ${BIONANO_FILES_DIR}/hybrid* .
 chmod +x hybrid*
 ln -s $ASSEMBLY asm.fasta
 ln -s $CMAP_FILE DL1.cmap
@@ -15,7 +15,7 @@ conda activate bionano
 
 # === STEP 1/:  ======================
 BIONANO_JOB
-sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --cpus-per-task=${CPUS} --mem=${MEM} --time=${MAX_TIME} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err "${BIONANO_DIR}/hybrid_scaffold_3.3.sh" ${ASSEMBLY_NAME} DLE1 "${BIONANO_DIR}/hybridScaffold_DLE1_config_3.3.xml"
+sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --cpus-per-task=${CPUS} --mem=${MEM} --time=${MAX_TIME} --output=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.out --error=${OUT_DIR}/${ASSEMBLY_NAME}/std_logs/%x.%j.err "${BIONANO_FILES_DIR}/hybrid_scaffold_3.3.sh" ${ASSEMBLY_NAME} DLE1 "${BIONANO_FILES_DIR}/hybridScaffold_DLE1_config_3.3.xml"
 BIONANO_JOB_ID=$(echo $BIONANO_JOB | cut -d ' ' -f4)
 
 # === STEP 2/:  ======================
