ed1ff3693f7d33b73c44b4042b861a97de612630
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-24 17:36:33 +0100

    Update run_arima_pipeline.sh

--- run_arima_pipeline.sh
+++ run_arima_pipeline.sh
@@ -7,27 +7,21 @@ mkdir -p "${OUT_DIR}/idx"
 mkdir -p ${TMPDIR}
 
 
-# === STEP 1/14: Reference index  ====================================
+# === STEP 1-2/14: Reference index - BWA index  ======================
 
 INDEX1_JOB=$(sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.index1.job)
 INDEX1_JOB_ID=$(echo $INDEX1_JOB | cut -d ' ' -f4)
 
 
-# === STEP 2/14: BWA index  ==========================================
-
-INDEX2_JOB=$(sbatch --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.index2.job)
-INDEX2_JOB_ID=$(echo $INDEX2_JOB | cut -d ' ' -f4)
-
-
 # === STEP 3/14: Map forward reads ===================================
 
-MAP1_JOB=$(sbatch --dependency=afterok:${INDEX1_JOB_ID}:${INDEX2_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.map1.job)
+MAP1_JOB=$(sbatch --dependency=afterok:${INDEX1_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.map1.job)
 MAP1_JOB_ID=$(echo $MAP1_JOB | cut -d ' ' -f4)
 
 
 # === STEP 4/14: Map reverse reads ===================================
 
-MAP2_JOB=$(sbatch --dependency=afterok:${INDEX1_JOB_ID}:${INDEX2_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.map2.job)
+MAP2_JOB=$(sbatch --dependency=afterok:${INDEX1_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.map2.job)
 MAP2_JOB_ID=$(echo $MAP2_JOB | cut -d ' ' -f4)
 
 
@@ -63,24 +57,24 @@ DEDUP_JOB_ID=$(echo $DEDUP_JOB | cut -d ' ' -f4)
 
 # === STEP 10/14: Index .bam =========================================
 
-INDEX_JOB=$(sbatch --dependency=afterok:${DEDUP_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.index.job)
-INDEX_JOB_ID=$(echo $INDEX_JOB | cut -d ' ' -f4)
+INDEX2_JOB=$(sbatch --dependency=afterok:${DEDUP_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.index2.job)
+INDEX2_JOB_ID=$(echo $INDEX2_JOB | cut -d ' ' -f4)
 
 
 # === STEP 11/14: Make stats =========================================
 
-STATS_JOB=$(sbatch --dependency=afterok:${INDEX_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.stats.job)
+STATS_JOB=$(sbatch --dependency=afterok:${INDEX2_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.stats.job)
 STATS_JOB_ID=$(echo $STATS_JOB | cut -d ' ' -f4)
 
 
 # === STEP 12/14: Remove intermediate files ==========================
 
-REMOVE_JOB=$(sbatch --dependency=afterok:${INDEX_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.clean.job)
+REMOVE_JOB=$(sbatch --dependency=afterok:${INDEX2_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.clean.job)
 
 
 # === STEP 13/14: Generate .bed from .bam ==========================
 
-BED1_JOB=$(sbatch --dependency=afterok:${INDEX_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.bed1.job)
+BED1_JOB=$(sbatch --dependency=afterok:${INDEX2_JOB_ID} --mail-user=${USER_MAIL} --partition=${PARTITION} --qos=${QUEUE} --output=${OUT_DIR}/std_logs/%x.%j.out --error=${OUT_DIR}/std_logs/%x.%j.err slurm/arima.bed1.job)
 BED1_JOB_ID=$(echo $BED1_JOB | cut -d ' ' -f4)
 
 
